9040fd1369936efd7ee42036f0ea84899425d35d
Suraj-000 <01fe20bec246@kletech.ac.in>
2023-04-08 19:25:58 +0530

    13

--- The_Shops_Arrangement.cpp
+++ The_Shops_Arrangement.cpp
@@ -1,65 +1,55 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<string>
-#include<stack>
-#include<queue>
-#include<map>
-#include<utility>
-#include<cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-#define ll long long int 
-#define yo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-
-using namespace std;
-
-int main(){
-    yo;
-    int t;
-    cin>>t;
-    while(t--){
-        int n;cin>>n;
-        vector<int> arr(n);
-        for(int i=0;i<n;i++)cin>>arr[i];
-        int q;cin>>q;
-        while(q--){
-            int type;cin>>type;
-            if(type==1){
-                int x,k;
-                cin>>x>>k;
-                arr[x-1]=k;
-            }else{
-                int x,y,k;cin>>x>>y>>k;
-                if(x==y){
-                    if(k>1)cout<<"0\n";
-                    else cout<<arr[x-1]<<endl;
-                }else if(k>y-x+1)cout<<"0\n";
-                else{
-                    int max=*max_element(arr.begin()+x-1,arr.begin()+y);
-                    int a[max+1];
-                    memset(a,0,(max+1)*sizeof(int));
-                    for(int i=x-1;i<y;i++)a[arr[i]]++;
-                    int count=0,flag=0;
-                    for(int i=0;i<=max;i++){
-                        while(a[i]>0){
-                            count++;
-                            if(count==k){
-                                cout<<i<<endl;
-                                flag=1;
-                                break;
-                            }
-                            a[i]--;
-                        }
-                        if(flag==1)break;
-                    }
-                }
-                vector<int> v;
-                for(int i=x-1;i<=y-1;i++)v.push_back(arr[i]);
-                sort(v.begin(),v.end());
-                if(k>=x && k<=y)cout<<v[k-1]<<endl;                
-                else cout<<0<<endl;
-            }
+int find_ranked_shop_profit(int *profits, int n, int x, int y, int k) {
+    int rank_count[n + 1], min_profit = INT_MAX, max_profit = INT_MIN;
+    for (int i = x - 1; i <= y - 1; i++) {
+        min_profit = profits[i] < min_profit ? profits[i] : min_profit;
+        max_profit = profits[i] > max_profit ? profits[i] : max_profit;
+    }
+    for (int i = 0; i <= n; i++) rank_count[i] = 0;
+    for (int i = x - 1; i <= y - 1; i++) rank_count[profits[i] - min_profit]++;
+    for (int i = 0, count = 0; i <= max_profit - min_profit; i++) {
+        count += rank_count[i];
+        if (count >= k) {
+            return i + min_profit;
         }
     }
     return 0;
 }
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+
+        int profits[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &profits[i]);
+        }
+
+        int q;
+        scanf("%d", &q);
+
+        for (int i = 0; i < q; i++) {
+            int query_type;
+            scanf("%d", &query_type);
+
+            if (query_type == 1) {
+                int x, k;
+                scanf("%d %d", &x, &k);
+                profits[x - 1] = k;
+            } else if (query_type == 0) {
+                int x, y, k;
+                scanf("%d %d %d", &x, &y, &k);
+                int result = find_ranked_shop_profit(profits, n, x, y, k);
+                printf("%d\n", result);
+            }
+        }
+    }
+    return 0;
+} 
